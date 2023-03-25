84c73c882507e42365a4fd3ac7397dc47380f479
Sagar <worthless.dumboo@gmail.com>
2023-03-25 18:59:26 -0500

    Lab7: Part2 ProductServic not discovered by eureka

--- Lab7/ProductService/src/main/java/com/example/ProductService/ProductServiceApplication.java
+++ Lab7/ProductService/src/main/java/com/example/ProductService/ProductServiceApplication.java
@@ -1,14 +1,19 @@
 package com.example.ProductService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- Lab7/ProductService/src/main/java/com/example/ProductService/integration/StockServiceFeignClient.java
+++ Lab7/ProductService/src/main/java/com/example/ProductService/integration/StockServiceFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "stock-service", url = "http://localhost:8900/api/v1/stock-service")
+@FeignClient(name = "StockService", url = "http://localhost:8900/api/v1/stock-service")
 public interface StockServiceFeignClient {
 
     @RequestMapping("/{productNo}")
